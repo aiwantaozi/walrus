66f6457febdcba43df70848db05fd7a5045c1bbb
michelia <michelia@seal.io>
2024-03-29 11:49:56 +0800

    catalog and template

--- pkg/apis/walruscore/v1/catalog.go
+++ pkg/apis/walruscore/v1/catalog.go
@@ -20,15 +20,51 @@ type Catalog struct {
 
 var _ runtime.Object = (*Catalog)(nil)
 
-// CatalogSpec defines the desired state of Catalog.
 type CatalogSpec struct {
-	// TODO: your spec here
+}
+type CatalogStatus struct {
 }
 
-// CatalogStatus defines the observed state of Catalog.
-type CatalogStatus struct {
-	// TODO: your status here
-}
+//// CatalogSpec defines the desired state of Catalog.
+//type CatalogSpec struct {
+//	// TemplateFormat of the catalog.
+//	// +required
+//	TemplateFormat string `json:"templateFormat"`
+//
+//	// Description of the catalog.
+//	// +optional
+//	Description string `json:"description,omitempty"`
+//
+//	// Filtering specifies the filtering rules for the catalog.
+//	// +optional
+//	Filtering *Filtering `json:"filtering,omitempty"`
+//
+//	// VCSSource specifies the vcs source configure.
+//	// +optional
+//	VCSSource *VCSSource `json:"vcsSource,omitempty"`
+//
+//	// HelmRepositorySource specifies the helm repository configure.
+//	// +optional
+//	HelmRepositorySource *HelmRepositorySource `json:"helmRepositorySource,omitempty"`
+//
+//	// OCIRegistrySource specifies the OCI registry configure.
+//	// +optional
+//	OCIRegistrySource *OCIRegistrySource `json:"ociRegistrySource,omitempty"`
+//}
+//
+//// CatalogStatus defines the observed state of Catalog.
+//type CatalogStatus struct {
+//	// LastSyncTime record the last auto/manual sync catalog time.
+//	LastSyncTime meta.Time `json:"lastSyncTime"`
+//
+//	// TemplateCount include template count.
+//	TemplateCount int `json:"templateCount"`
+//
+//	// Conditions holds the conditions for the catalog.
+//	// +optional
+//	Conditions []meta.Condition `json:"conditions,omitempty"`
+//}
+//
 
 // CatalogList holds the list of Catalog.
 //
@@ -41,3 +77,74 @@ type CatalogList struct {
 }
 
 var _ runtime.Object = (*CatalogList)(nil)
+
+// Filtering specifies the filtering rules for the catalog's template.
+type Filtering struct {
+	// IncludeFilter specifies the regular expression for matching the include template name.
+	// +optional
+	IncludeFilter string `json:"includeFilter,omitempty"`
+
+	// ExcludeFilter specifies the regular expression for matching the exclude template name.
+	// +optional
+	ExcludeFilter string `json:"excludeFilter,omitempty"`
+}
+
+// VCSSource specifies the vcs source configure.
+type VCSSource struct {
+	// TokenAuth containing authentication credentials for vcs source.
+	TokenAuth `json:",inline"`
+
+	// +k8s:validation:enum=["github","gitlab","gitee"]
+	// +required
+	Platform string `json:"platform"`
+
+	// URL of the source address, a valid URL contains at least a protocol and host.
+	// +required
+	URL string `json:"url"`
+}
+
+// OCIRegistrySource specifies the OCI registry configure.
+type OCIRegistrySource struct {
+	// BasicAuth containing authentication credentials for OCI registry.
+	BasicAuth `json:",inline"`
+
+	// Domain for OCI registry.
+	// +optional
+	Domain string `json:"domain,omitempty"`
+}
+
+// HelmRepositorySource specifies the helm repository configure.
+type HelmRepositorySource struct {
+	// BasicAuth containing authentication credentials for helm repository.
+	BasicAuth `json:",inline"`
+
+	// URL of the source address, a valid URL contains at least a protocol and host.
+	// +required
+	URL string `json:"url"`
+}
+
+type BasicAuth struct {
+	// Username for OCI registry.
+	// +optional
+	Username string `json:"username,omitempty"`
+
+	// Password for OCI registry.
+	// +optional
+	Password string `json:"password,omitempty"`
+
+	// SecretRef specifies the Secret containing authentication credentials for helm repository.
+	// For HTTP/S basic auth the secret must contain 'username' and 'password' fields.
+	// +optional
+	SecretRef *ObjectReference `json:"authSecretRef,omitempty"`
+}
+
+type TokenAuth struct {
+	// Token for HTTP/S bear token.
+	// +optional
+	Token string `json:"token,omitempty"`
+
+	// SecretRef specifies the Secret containing authentication credentials.
+	// For HTTP/S bear token the secret must contain 'token' field.
+	// +optional
+	SecretRef *ObjectReference `json:"authSecretRef,omitempty"`
+}
--- pkg/apis/walruscore/v1/template.go
+++ pkg/apis/walruscore/v1/template.go
@@ -20,15 +20,49 @@ type Template struct {
 
 var _ runtime.Object = (*Template)(nil)
 
-// TemplateSpec defines the desired state of Template.
 type TemplateSpec struct {
-	// TODO: your spec here
 }
 
-// TemplateStatus defines the observed state of Template.
 type TemplateStatus struct {
-	// TODO: your status here
 }
+
+//// TemplateSpec defines the desired state of Template.
+//type TemplateSpec struct {
+//	// TemplateFormat of the content.
+//	// +required
+//	TemplateFormat string `json:"templateFormat"`
+//
+//	// Description of the template.
+//	// +optional
+//	Description string `json:"description,omitempty"`
+//
+//	// VCSRepository specifies the vcs repository configure.
+//	// +optional
+//	VCSRepository *VCSRepository `json:"vcsRepository,omitempty"`
+//
+//	// HelmOCIChart specifies the OCI format helm chart configure.
+//	// +optional
+//	HelmOCIChart *HelmOCIChart `json:"helmOCIChart,omitempty"`
+//
+//	// HelmChart specifies the tgz format helm chart configure.
+//	// +optional
+//	HelmChart *HelmChart `json:"helmChart,omitempty"`
+//}
+
+// TemplateStatus defines the observed state of Template.
+//type TemplateStatus struct {
+//	// The original name of the template, name generate from chart.yaml, terraform git repo name.
+//	OriginalName string `json:"originalName,omitempty"`
+//
+//	// A URL to an SVG or PNG image to be used as an icon.
+//	Icon string `json:"icon,omitempty"`
+//
+//	// Versions holds the versions for the template.
+//	Versions []TemplateVersion `json:"versions,omitempty"`
+//
+//	// Conditions holds the conditions for the template.
+//	Conditions []meta.Condition `json:"conditions,omitempty"`
+//}
 
 // TemplateList holds the list of Template.
 //
@@ -41,3 +75,51 @@ type TemplateList struct {
 }
 
 var _ runtime.Object = (*TemplateList)(nil)
+
+// VCSRepository specifies the vcs repository of the template.
+type VCSRepository struct {
+	TokenAuth `json:",inline"`
+
+	// URL of download the template from vsc repository with ref and subpath, e.g. https://github.com/walrus-catalog/terraform-static-redis?ref=main.
+	// +required
+	URL string `json:"url"`
+}
+
+// HelmOCIChart specifies the OCI format helm chart configure.
+type HelmOCIChart struct {
+	// BasicAuth containing authentication credentials for oci chart.
+	BasicAuth `json:",inline"`
+
+	// URL of download the template from oci image with ref, e.g. oci://registry-1.docker.io/bitnamicharts/mysql?ref=10.1.0.
+	// +required
+	URL string `json:"url"`
+}
+
+// HelmChart specifies the tgz format helm chart configure.
+type HelmChart struct {
+	// BasicAuth containing authentication credentials for helm chart.
+	BasicAuth `json:",inline"`
+
+	// URL of download the template from chart tgz address, e.g. https://charts.bitnami.com/bitnami/phpbb-16.5.0.tgz.
+	// +required
+	URL string `json:"url"`
+}
+
+// TemplateVersion defines the version of Template.
+type TemplateVersion struct {
+	Version string `json:"version,omitempty"`
+
+	// URL of downloading the version.
+	// host.
+	// +required
+	URL string `json:"url"`
+
+	// Schema holds the schema for the template version.
+	Schema *ObjectReference `json:"schema,omitempty"`
+
+	// OriginalUISchema holds the original UI schema for the template version.
+	OriginalUISchema *ObjectReference `json:"originalUISchema,omitempty"`
+
+	// UISchema holds the UI schema for the template version.
+	UISchema *ObjectReference `json:"uiSchema,omitempty"`
+}
